6ead9a5d73184d844d1f261b26f0d221496ad260
ushergodwin <godwintumuhimbise96@gmail.com>
2022-10-19 17:39:23 +0300

    gh-2 created classes for the bot object

---
+++ telegram/objects/botcommand.py
@@ -0,0 +1,25 @@
+class BotCommand:
+    pass
+
+class BotCommandScope:
+    pass 
+
+class BotCommandScopeAllChatAdministrattos:
+    pass 
+
+class BotCommandScopeAllGroupChats:
+    pass 
+
+class BotCommandScopeAllPrivatechats:
+    pass
+class BotCommandScopeChat:
+    pass 
+
+class BotCommandScopeChatAdministrators:
+    pass 
+
+class BotCommandScopeChatMember:
+    pass 
+
+class BotCommandScopeDefault:
+    pass 
