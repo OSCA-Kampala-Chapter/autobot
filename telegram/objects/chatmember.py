3f1ce06e806c6a45195bb089f53704a72f8d192d
ht-thomas <higenyi.tobias@gmail.com>
2022-10-22 12:21:41 +0300

    gh-6 created classes for chatmember related objects

---
+++ telegram/objects/chatmember.py
@@ -0,0 +1,24 @@
+
+class ChatMember:
+    pass
+    
+class ChatMemberAdministrator:
+    pass
+    
+class ChatMemberBanned:
+    pass
+    
+class ChatMemberLeft:
+    pass
+    
+class ChatMemberMember:
+    pass
+    
+class ChatMemberOwner:
+    pass
+    
+class ChatMemberRestricted:
+    pass
+    
+class ChatMemberUpdated:
+    pass
