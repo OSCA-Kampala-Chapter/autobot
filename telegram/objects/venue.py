a45db175174165c29af94e129bafdb5236d55c01
edger-asiimwe <edgerasiimwe@gmail.com>
2022-10-24 22:33:05 +0300

    Added properties for the Venue class

--- telegram/objects/venue.py
+++ telegram/objects/venue.py
@@ -1,2 +1,18 @@
 class Venue:
-    pass 
+    def __init__(self,
+        location,
+        title,
+        address,
+        foursquare_id = None,
+        foursquare_type = None,
+        google_place_id = None,
+        google_place_type = None,
+        ):
+
+        self.location = location
+        self.title = title
+        self.address = address
+        self.foursquare_id = foursquare_id
+        self.foursquare_type = foursquare_type
+        self.google_place_id = google_place_id
+        self.google_place_type = google_place_type
