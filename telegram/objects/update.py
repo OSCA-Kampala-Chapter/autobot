19e2c53d37bc6e67b249219b2d872eac5afd1f7d
ht-thomas <higenyi.tobias@gmail.com>
2022-10-25 01:39:54 +0300

    gh-1 created update module

---
+++ telegram/objects/update.py
@@ -0,0 +1,33 @@
+class Update:
+    def __init__ (self,
+        update_id,
+        message = None,
+        edited_message = None,
+        channel_post = None,
+        edited_channel_post = None,
+        inline_query = None,
+        chosen_inline_result = None,
+        callback_query = None,
+        shipping_query = None,
+        pre_checkout_query = None,
+        poll = None,
+        poll_answer = None,
+        my_chat_member = None,
+        chat_member = None,
+        chat_join_request = None,
+    ):
+        self.update_id = update_id
+        self.message = message
+        self.edited_message = edited_message
+        self.channel_post = channel_post
+        self.edited_channel_post = edited_channel_post
+        self.inline_query = inline_query
+        self.chosen_inline_result = chosen_inline_result
+        self.callback_query = callback_query
+        self.shipping_query = shipping_query
+        self.pre_checkout_query = pre_checkout_query
+        self.poll = poll
+        self.poll_answer = poll_answer
+        self.my_chat_member = my_chat_member
+        self.chat_member = chat_member
+        self.chat_join_request = chat_join_request
