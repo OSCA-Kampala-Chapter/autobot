042d8990a0e86f027bddcea7a0717371fe45fb53
edger-asiimwe <edgerasiimwe@gmail.com>
2022-10-25 10:29:44 +0300

    Added properties for the Video module

--- telegram/objects/video.py
+++ telegram/objects/video.py
@@ -1,17 +1,60 @@
 class Video:
-    pass
+    def __init__(self,
+        file_id,
+        file_unique_id,
+        width,
+        height,
+        duration,
+        thumb = None,
+        file_name = None,
+        mime_type = None,
+        file_size = None
+        ):
+
+        self.file_id = file_id
+        self.file_unique_id = file_unique_id
+        self.width = width
+        self.height = height
+        self.duration = duration
+        self.thumb = thumb
+        self.file_name = file_name
+        self.mime_type = mime_type
+        self.file_size = file_size
     
 class VideoChatEnded:
-    pass
+    def __init__(self, duration):
+        self.duration = duration
     
 class VideoChatParticipantsInvited:
-    pass
+    def __init__(self, users):
+        self.users = users
+
     
 class VideoChatScheduled:
-    pass
+    def __init__(self, start_date):
+        self.start_date = start_date
+
     
 class VideoChatStarted:
+    #This object represents a service message about a video chat started in the chat. 
+    #
+    #Currently holds no information.
     pass
+
     
 class VideoNote:
-    pass
+    def __init__(self,
+        file_id,
+        file_unique_id,
+        length,
+        duration,
+        thumb = None,
+        file_size = None
+        ):
+
+        self.file_id = file_id
+        self.file_unique_id = file_unique_id
+        self.length = length
+        self.duration = duration
+        self.thumb = thumb
+        self.file_size = file_size
