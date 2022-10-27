62c358f09508ad96dfa3043895d9dde1a216e369
ht-thomas <higenyi.tobias@gmail.com>
2022-10-27 15:23:11 +0300

    Update chat.py

--- telegram/objects/chat.py
+++ telegram/objects/chat.py
@@ -1,7 +1,7 @@
 class Chat:
     def __init__(self, 
-        id = None,
-        type = None,
+        id,
+        type,
         title = None,
         username = None,
         first_name = None,
@@ -22,30 +22,31 @@ class Chat:
         sticker_set_name = None,
         can_set_sticker_set = None,
         linked_chat_id = None,
-        location = None ):
-            self.id = id
-            self.type = type
-            self.title = title
-            self.username = username
-            self.first_name = first_name
-            self.last_name = last_name
-            self.photo = photo
-            self.bio = bio
-            self.has_private_forwards = has_private_forwards
-            self.has_restricted_voice_and_video_messages = has_restricted_voice_and_video_messages
-            self.join_to_send_messages = join_to_send_messages
-            self.join_by_request = join_by_request
-            self.description = description
-            self.invite_link = invite_link
-            self.pinned_message = pinned_message
-            self.permissions = permissions
-            self.slow_mode_delay = slow_mode_delay
-            self.message_auto_delete_time = message_auto_delete_time
-            self.has_protected_content = has_protected_content
-            self.sticker_set_name = sticker_set_name
-            self.can_set_sticker_set = can_set_sticker_set
-            self.linked_chat_id = linked_chat_id
-            self.location = location
+        location = None
+    ):
+        self.id = id
+        self.type = type
+        self.title = title
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+        self.photo = photo
+        self.bio = bio
+        self.has_private_forwards = has_private_forwards
+        self.has_restricted_voice_and_video_messages = has_restricted_voice_and_video_messages
+        self.join_to_send_messages = join_to_send_messages
+        self.join_by_request = join_by_request
+        self.description = description
+        self.invite_link = invite_link
+        self.pinned_message = pinned_message
+        self.permissions = permissions
+        self.slow_mode_delay = slow_mode_delay
+        self.message_auto_delete_time = message_auto_delete_time
+        self.has_protected_content = has_protected_content
+        self.sticker_set_name = sticker_set_name
+        self.can_set_sticker_set = can_set_sticker_set
+        self.linked_chat_id = linked_chat_id
+        self.location = location
 
 
     
