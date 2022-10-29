94dc0f37c11702c8d45febd92c225c7fe82517f4
edger-asiimwe <edgerasiimwe@gmail.com>
2022-10-29 22:57:47 +0300

    Added properties for Document class

--- telegram/objects/document.py
+++ telegram/objects/document.py
@@ -1,4 +1,18 @@
 from .base import BaseObject
 
 class Document(BaseObject):
-    pass 
+    def __init__(self,
+        file_id,
+        file_unique_id,
+        thumb = None,
+        file_name = None,
+        mime_type = None,
+        file_size = None
+        ):
+
+        self.file_id = file_id
+        self.file_unique_id = file_unique_id
+        self.thumb = thumb
+        self.file_name = file_name
+        self.mime_type = mime_type
+        self.file_size = file_size
