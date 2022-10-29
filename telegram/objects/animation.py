1a6eb00d121f2cfdd90cfabe28ad8494de028766
edger-asiimwe <edgerasiimwe@gmail.com>
2022-10-29 22:54:17 +0300

    Added properties for Animation class

--- telegram/objects/animation.py
+++ telegram/objects/animation.py
@@ -1,4 +1,24 @@
 from .base import BaseObject
 
 class Animation (BaseObject):
-    pass
+    def __init__(self,
+        file_id,
+        file_unique_id,
+        width,
+        height,
+        duration,
+        thumb = None,
+        file_name = None,
+        mime_type = None,
+        file_size = None
+        ):
+
+        self.file_id = file_id
+        self.file_unique_id = file_unique_id
+        self.width = width
+        self.height = height
+        self.duration = duration
+        self.thumb = thumb
+        self.file_name = file_name
+        self.mime_type = mime_type
+        self.file_size = file_size
