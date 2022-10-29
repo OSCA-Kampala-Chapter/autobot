20f6d4333585b61ccef9bad08b1485e608f6e30b
edger-asiimwe <edgerasiimwe@gmail.com>
2022-10-29 22:50:07 +0300

    Added properties for PhotoSize

--- telegram/objects/photosize.py
+++ telegram/objects/photosize.py
@@ -1,4 +1,16 @@
 from .base import BaseObject
 
 class PhotoSize(BaseObject):
-    pass
+    def __init__(self,
+        file_id,
+        file_unique_id,
+        width,
+        height,
+        file_size = None
+        ):
+
+        self.file_id = file_id
+        self.file_unique_id = file_unique_id
+        self.width = width
+        self.height = height
+        self.file_size = file_size
