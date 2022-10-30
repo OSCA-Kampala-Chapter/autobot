401d2c1fa059927b163782dea658f34798bf90c0
Kally <60466044+kallyas@users.noreply.github.com>
2022-10-30 16:27:36 +0300

    Update audio.py

--- telegram/objects/audio.py
+++ telegram/objects/audio.py
@@ -6,13 +6,22 @@ class Audio(BaseObject):
 
     Args:
         file_id (str): Unique identifier for this file
-        file_unique_id (str): Unique identifier for this file, which is supposed to be the same over time and for different bots. Can't be used to download or reuse the file.
+        
+        file_unique_id (str): Unique identifier for this file, which is supposed to be the same over time and for different bots. 
+        Can't be used to download or reuse the file.
+        
         duration (int): Optional Duration of the audio in seconds as defined by sender
+        
         performer (str): Optional. Performer of the audio as defined by sender or by audio tags
+        
         title (str): Optional. Title of the audio as defined by sender or by audio tags
+        
         file_name (str): Optional. Original filename as defined by sender
+        
         mime_type (str): Optional. MIME type of the file as defined by sender
+        
         file_size (int): Optional. File size
+        
         thumb (:obj:`PhotoSize`, optional): Optional. Thumbnail of the album cover to which the music file belongs
     """
 
