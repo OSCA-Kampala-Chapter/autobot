1487e902974a832411104c2650c6a62991c77700
Kally <60466044+kallyas@users.noreply.github.com>
2022-10-30 16:26:58 +0300

    Update audio.py

--- telegram/objects/audio.py
+++ telegram/objects/audio.py
@@ -16,7 +16,15 @@ class Audio(BaseObject):
         thumb (:obj:`PhotoSize`, optional): Optional. Thumbnail of the album cover to which the music file belongs
     """
 
-    __slots__ = ('duration', 'performer', 'title', 'file_name', 'mime_type', 'file_size', 'thumb')
+    __slots__ = (
+        'duration', 
+        'performer', 
+        'title', 
+        'file_name', 
+        'mime_type', 
+        'file_size', 
+        'thumb'
+    )
 
     def __init__(self, file_id, file_unique_id):
         self.file_id = file_id
