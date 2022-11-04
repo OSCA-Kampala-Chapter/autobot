ed833be35592caee2e6405b5473183f7cbc25c21
kallyas <kallyasmedia@gmail.com>
2022-11-04 11:18:41 +0300

    gh-50: typing game completed

--- autobot/telegram/games/objects.py
+++ autobot/telegram/games/objects.py
@@ -1,7 +1,8 @@
-from ..objects import BaseObject
+from ..objects import BaseObject, PhotoSize, MessageEntity, User, Animation
+from typing import Optional
 
-class Game (BaseObject):
-
+class Game(BaseObject):
+    
     __slots__ = ("title",
                 "description",
                 "photo",
@@ -9,23 +10,29 @@ class Game (BaseObject):
                 "text_entities",
                 "animation",
                 )
-    def __init__ (self,title,description,photo):
+
+    def __init__(self, title: str, description: str, photo: list[PhotoSize]) -> None:
         self.title = title
         self.description = description
         self.photo = photo
-        self.text = None
-        self.text_entities = None
-        self.animation = None
-        
-        
-class GameHighScore (BaseObject):
-    
+        self.text: Optional[str] = None
+        self.text_entities: Optional[list[MessageEntity]] = None
+        self.animation: Optional[Animation] = None
+
+
+class GameHighScore(BaseObject):
+
     __slots__ = ("position",
                 "user",
                 "score",
                 )
-    def __init__ (self,position,user,score):
+
+
+    def __init__(self, position: int, user: User, score: int) -> None:
         self.position = position
         self.user = user
-        self.score = score
+        self.score = score     
+
+
+
         
