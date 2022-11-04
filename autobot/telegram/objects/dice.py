d4ca6d4b465f6d765a0c5336b6a963341cc104e3
edger-asiimwe <edgerasiimwe@gmail.com>
2022-11-04 10:56:04 +0300

    Created dice class and typing + documentation

--- autobot/telegram/objects/dice.py
+++ autobot/telegram/objects/dice.py
@@ -1,4 +1,19 @@
 from .base import BaseObject
 
 class Dice(BaseObject):
-    pass
+    
+    """
+        This object represents an animated emoji that displays a random value.
+
+        Args:
+            emoji (str) : Emoji on which the dice throw animation is based
+
+            value (int) : Value of the dice, 1-6 for “🎲”, “🎯” and “🎳” base emoji, 1-5 for “🏀” and “⚽” base emoji, 1-64 for “🎰” base emoji
+
+    """
+
+    __slots__ = ("emoji", "value",)
+
+    def __init__(self, emoji:str, value:int):
+        self.emoji = emoji
+        self.value = value
