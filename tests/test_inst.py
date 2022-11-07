c87503a95293b995ee29ec8261305ab593bee676
TobiasHT <higenyi.tobias@gmail.com>
2022-11-07 11:07:22 +0300

    add tests to autobot (#62)

---
+++ tests/test_inst.py
@@ -0,0 +1,7 @@
+import unittest
+from autobot.telegram.objects import *
+from autobot.telegram.games.objects import *
+from autobot.telegram.inline.objects import *
+from autobot.telegram.passport.objects import *
+from autobot.telegram.payments.objects import *
+from autobot.telegram.stickers.objects import *
