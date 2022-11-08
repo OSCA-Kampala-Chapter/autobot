2c2cc231c64ae55647f2a677cb2ba364b86456a2
TobiasHT <higenyi.tobias@gmail.com>
2022-11-08 17:11:00 +0300

    create parser (#65)

---
+++ autobot/telegram/parser.py
@@ -0,0 +1,27 @@
+from autobot.telegram.objects import *
+from autobot.telegram.games.objects import *
+from autobot.telegram.inline.objects import *
+from autobot.telegram.passport.objects import *
+from autobot.telegram.payments.objects import *
+from autobot.telegram.stickers.objects import *
+
+class Parser:
+    
+    # methods to parse general telegram objects
+    
+    
+    # methods to parse games objects
+    
+    
+    # methods to parse inline objects
+    
+    
+    # methods to parse passport objects
+    
+    
+    # methods to parse payment objects
+    
+    
+    # methods to parse sticker objects
+    
+    
