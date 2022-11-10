ca9f2074630f72899a1c75e029ed7e97c8e692a0
kallyas <kallyasmedia@gmail.com>
2022-11-10 13:49:47 +0300

    ch: implement forum objects

--- autobot/telegram/objects/forumtopic.py
+++ autobot/telegram/objects/forumtopic.py
@@ -1,11 +1,36 @@
 from autobot.telegram.objects import BaseObject
+from typing import Optional
 
 class ForumTopicCreated (BaseObject):
-    pass
+    """
+    This object represents a service message about a new forum topic created in the chat
+
+    <https://core.telegram.org/bots/api#forumtopiccreated>
+
+    Args:
+        name: (str) Name of the topic
+        icon_color: (int) Color of the topic icon in RGB format
+        icon_custom_emoji_id: (str) Optional. Unique identifier of the custom emoji shown as the topic icon
+    """
+
+    __slots__ = (
+        'name',
+        'icon_color',
+        'icon_custom_emoji_id'
+    )
     
+    def __init__(self, name: str = None, icon_color: int = None) -> None:
+        self.name = name
+        self.icon_color = icon_color
+        self.icon_custom_emoji_id: Optional[str] = None
 class ForumTopicClosed (BaseObject):
+    """
+    This object represents a service message about a forum topic closed in the chat. Currently holds no information.
+    """
     pass
     
 class ForumTopicReopened (BaseObject):
+    """
+    This object represents a service message about a forum topic reopened in the chat. Currently holds no information.
+    """
     pass
-    
