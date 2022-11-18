4d02ffa8034b8d91b745c61a3846c75b6a6bc93a
ht-thomas <higenyi.tobias@gmail.com>
2022-11-18 07:46:37 +0300

    gh-99 initiate api classes

---
+++ autobot/telegram/games/api.py
@@ -0,0 +1,5 @@
+"""
+This module contains a class with methods representing the games api
+"""
+class GamesAPI:
+    pass
---
+++ autobot/telegram/payments/api.py
@@ -0,0 +1,5 @@
+"""
+This module contains a class with methods representing the payments api
+"""
+class PaymentsAPI:
+    pass
