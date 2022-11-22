b1312e06c12955a845647eb19ac6fc916e3a96b0
ht-thomas <higenyi.tobias@gmail.com>
2022-11-22 18:20:00 +0300

    set up NAL infrastructure

---
+++ autobot/network/protocol.py
@@ -0,0 +1,16 @@
+"""
+The Protocol module contains abstract classes that represent a protocol.
+The Classes expose specific methods that define the relevant protocol
+and can be used to send and receive requests. 
+The Concrete subclasses should implement the given methods
+"""
+
+class HTTP:
+    """
+    Base abstraction class for the HTTP Protocol
+    """
+
+class WebSocket:
+    """
+    Base abstraction class for the Websocket protocol
+    """
