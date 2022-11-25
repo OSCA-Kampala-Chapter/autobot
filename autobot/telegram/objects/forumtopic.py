982659a7aa50e89b0787a0cef4f24c0fdceb1120
ushergodwin <godwintumuhimbise96@gmail.com>
2022-11-25 12:37:51 +0300

    added missing object

--- autobot/telegram/objects/forumtopic.py
+++ autobot/telegram/objects/forumtopic.py
@@ -1,6 +1,30 @@
-from autobot.telegram.objects import BaseObject
 from typing import Optional
 
+from autobot.telegram.objects import BaseObject
+
+
+class ForumTopic(BaseObject):
+    
+    def __init__(self, message_thread_id: int, name:str, icon_color:int) -> None:
+        
+        __slots__  = (
+            "message_thread_id",
+            "name",
+            "icon_color",
+            "icon_custom_emoji_id"
+        )
+        """This object represents a forum topic.
+
+        Args:
+            message_thread_id (int): Unique identifier of the forum topic
+            name (str): Name of the topic
+            icon_color (int): Color of the topic icon in RGB format
+            icon_custom_emoji_id (str, Optional): Unique identifier of the custom emoji shown as the topic icon
+        """
+        self.message_thread_id = message_thread_id
+        self.name = name
+        self.icon_custom_emoji_id: Optional[str] = None 
+        
 class ForumTopicCreated (BaseObject):
     """
     This object represents a service message about a new forum topic created in the chat
