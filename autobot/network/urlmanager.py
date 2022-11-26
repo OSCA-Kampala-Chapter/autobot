d74e5fffa8c050ea8d552dee8b24a9135d6cda6a
ht-thomas <higenyi.tobias@gmail.com>
2022-11-26 08:45:19 +0300

    Create urlmanager.py

---
+++ autobot/network/urlmanager.py
@@ -0,0 +1,33 @@
+"""
+This module handles construction of urls needed for making queries
+"""
+from urllib.parse import urlsplit,urlunsplit,urljoin
+
+__all__ = ("UrlManager")
+
+class UrlManager:
+
+    def __init__ (self,token):
+        self.token = "bot" + str(token) + "/"
+        self.url_parts = ["https","api.telegram.org","","",""]
+        self.url = urljoin(urlunsplit(self.url_parts),self.token)
+
+    def add_method (self,other):
+        """
+        append other part onto the url string
+        """
+        sch,netloc,path,qry,frag = urlsplit(self.url)
+        prev,base,method = path.split("/")
+        path = "/".join([base,other])
+        self.url = urlunsplit((sch,netloc,path,qry,frag))
+        return self.url
+
+    def add_query (self,**kwargs):
+        """
+        append queries to the query part of the url
+        """
+        params = ["=".join([str(k),str(v)]) for k,v in kwargs.items()]
+        qry_string = "&".join(params)
+        sch,netloc,path,qry,frag = urlsplit(self.url)
+        qry = qry_string
+        return urlunsplit([sch,netloc,path,qry,frag])
