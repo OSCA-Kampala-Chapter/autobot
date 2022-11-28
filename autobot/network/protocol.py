54139b8dea27d132cfd583ac6824b20576de1010
ht-thomas <higenyi.tobias@gmail.com>
2022-11-28 16:39:15 +0300

    add get,post,close methods

--- autobot/network/protocol.py
+++ autobot/network/protocol.py
@@ -4,11 +4,31 @@ The Classes expose specific methods that define the relevant protocol
 and can be used to send and receive requests. 
 The Concrete subclasses should implement the given methods
 """
+from typing import Protocol
+from abc import abstractmethod
 
-class HTTP:
+class HTTP (Protocol):
     """
     Base abstraction class for the HTTP Protocol
     """
+
+    @abstractmethod
+    async def get (self,url:str,headers:dict = None) -> dict:
+        """
+        abstract get method
+        """
+
+    @abstractmethod
+    async def post (self,url:str,headers:dict = None,body:dict = None) -> dict:
+        """
+        abstract post method
+        """
+
+    @abstractmethod
+    async def close (self):
+        """
+        abstract close method
+        """
 
 class WebSocket:
     """
