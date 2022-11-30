0effcb9e0fb4bf8bc918b76d293f8e015a45e917
kallyas <kallyasmedia@gmail.com>
2022-11-30 11:14:04 +0300

    conn

--- autobot/network/connection.py
+++ autobot/network/connection.py
@@ -2,3 +2,9 @@
 This module contains specific implementations for the abstract protocols in the
 protocol module. The protocols are implemented as connection classes
 """
+
+class Connection(object):
+    """
+    This is the abstract base class for all connections. It defines the
+    interface that all connections must implement.
+    """
