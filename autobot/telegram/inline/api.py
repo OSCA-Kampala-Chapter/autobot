13d1d70bee0c2e42df31db41abfa68c9d833475b
kallyas <kallyasmedia@gmail.com>
2022-12-08 22:17:42 +0300

    ch: add games and inline apis

--- autobot/telegram/inline/api.py
+++ autobot/telegram/inline/api.py
@@ -3,4 +3,9 @@ This module contains a class with methods representing the inline api
 """
 
 class InlineAPI:
-    pass
+    
+    async def answer_inline_query(self, **kwargs):
+        url = self.url.add_method("answerInlineQuery")
+        res = await self._post(url=url, body=kwargs)
+        return self.parser.parse(res)
+
