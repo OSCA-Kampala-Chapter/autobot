b05dff274a6e29cfc1c8b41b147c15a258a84d7c
ht-thomas <higenyi.tobias@gmail.com>
2022-12-29 16:36:54 +0300

    migrate telegram to autotelegram

---
+++ autobot/telegram/__init__.py
@@ -0,0 +1,13 @@
+from autotelegram.telegram import (
+    games,
+    inline,
+    objects,
+    passport,
+    payments,
+    stickers,
+    api,
+    application,
+    channel,
+    context,
+    parser,
+)
--- autobot/telegram/objects/base.py
+++
@@ -1,2 +0,0 @@
-class BaseObject:
-    pass
